c5ee3de4f4e641255ea76068eef9076c42c00645
Hira-Yuki <cjsdlf44@gmail.com>
2023-08-18 16:41:03 +0900

    컴포넌트 파일 분리하여 저장

--- src/Pages/PlayGame.tsx
+++ src/Pages/PlayGame.tsx
@@ -1,65 +1,95 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import MenuBar from "components/feature/InGameMenu/MenuBar";
 import { story } from "data/Script/Sample";
+import ScriptDisplay from "components/feature/ScriptDisplay/ScriptDisplay";
 
 function PlayGame() {
   const navigate = useNavigate();
 
-  const [nameDisable, setNameDisable] = useState<boolean>(false);
+  // 현재 이야기 인덱스와 해당 인덱스의 캐릭터 이름과 스크립트를 상태로 관리
   const [displayIndex, setDisplayIndex] = useState<number>(0);
   const [charName, script]: string[] = story[displayIndex];
-
   const [currentText, setCurrentText] = useState<string>("");
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [animationPaused, setAnimationPaused] = useState<boolean>(false);
-  const [storyFinished, setStoryFinished] = useState<boolean>(false);
 
+  const isLastStory: boolean = displayIndex === story.length - 1;
+
+  // "다음 문장 " 클릭 처리 함수
   const handleNextClick = () => {
-    if (storyFinished) {
-      setDisplayIndex(displayIndex + 1);
-      setCurrentText("");
-      setCurrentIndex(0);
-      setStoryFinished(false);
-      setAnimationPaused(false);
-    } else if (animationPaused) {
-      setAnimationPaused(false);
-    } else if (currentIndex < script.length) {
-      setCurrentText(script);
-      setCurrentIndex(script.length);
+    // 애니메이션이 진행 중이면 텍스트를 모두 출력하고 일시정지
+    if (!animationPaused && currentIndex < script.length) {
+      completeTextAnimation();
     } else {
-      setAnimationPaused(true);
-      setStoryFinished(true);
+      proceedToNextAction();
     }
   };
 
-  useEffect(() => {
-    charName !== "" ? setNameDisable(false) : setNameDisable(true);
-  }, [charName]);
+  // 텍스트 애니메이션 완료 함수
+  const completeTextAnimation = () => {
+    setCurrentText(script);
+    setCurrentIndex(script.length);
+    setAnimationPaused(true);
+  };
+
+  // 다음 동작 수행 함수
+  const proceedToNextAction = () => {
+    setAnimationPaused(false);
+
+    if (currentIndex < script.length) {
+      completeTextAnimation();
+    } else {
+      if (isLastStory) {
+        handleStoryCompletion();
+      } else {
+        moveToNextStory();
+      }
+    }
+  };
+
+  // 이야기 완료 처리 함수
+  const handleStoryCompletion = () => {
+    alert("이야기가 끝났습니다.");
+    localStorage.setItem("isClear", "true");
+    navigate("/");
+  };
+
+  // 다음 이야기로 전환 함수
+  const moveToNextStory = () => {
+    setDisplayIndex(displayIndex + 1);
+    resetState();
+  };
+
+  // 상태 초기화 함수
+  const resetState = () => {
+    setCurrentText("");
+    setCurrentIndex(0);
+    setAnimationPaused(false);
+  };
 
   useEffect(() => {
+    // 애니메이션이 진행 중이고 텍스트 출력이 완료되지 않았으면 타이머 설정하여 한 글자씩 출력
     if (!animationPaused && currentIndex < script.length) {
       const timer = setTimeout(() => {
         setCurrentText((prevText) => prevText + script[currentIndex]);
         setCurrentIndex(currentIndex + 1);
       }, 50);
+      // 타이머 클리어 함수 반환으로 정리
       return () => clearTimeout(timer);
     }
-  }, [currentIndex, animationPaused]);
+  }, [currentIndex, animationPaused, script]);
 
   return (
     <StContainer>
       <StBottom>
         <StMiniMenuBar>
+          {/* 게임 내 메뉴 바 */}
           <MenuBar />
         </StMiniMenuBar>
-        <StScriptFeild onClick={handleNextClick}>
-          {nameDisable ? null : (
-            <StScriptCharName>〈{charName}〉</StScriptCharName>
-          )}
-          <StScriptText>{currentText}</StScriptText>
-        </StScriptFeild>
+        {/* 스크립트 출력 영역, 클릭하면 조건에 따라 동작 실행 */}
+        <ScriptDisplay charName={charName} currentText={currentText} onClick={handleNextClick} />
       </StBottom>
     </StContainer>
   );
@@ -67,6 +97,7 @@ function PlayGame() {
 
 export default PlayGame;
 
+// 컴포넌트 스타일 정의
 const StContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -91,21 +122,3 @@ const StMiniMenuBar = styled.div`
   gap: 10px;
   height: 14%;
 `;
-
-const StScriptFeild = styled.div`
-  margin: 0 1% 1% 1%;
-  padding: 1%;
-  height: 84%;
-  border: 1px solid black;
-
-  display: flex;
-  gap: 20px;
-
-  font-size: 32px;
-`;
-
-const StScriptCharName = styled.div`
-  font-weight: bold;
-`;
-
-const StScriptText = styled.span``;
---
+++ src/components/feature/ScriptDisplay/ScriptDisplay.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+
+interface TextDisplayProps {
+  charName: string;
+  currentText: string;
+  onClick: () => void;
+}
+
+const ScriptDisplay: React.FC<TextDisplayProps> = ({ charName, currentText, onClick }) => {
+  
+  // 캐릭터 이름이 비어있으면 이름 숨김 처리
+  const nameDisable: boolean = charName === "";
+
+  return (
+    <StScriptField onClick={onClick}>
+      {nameDisable ? null : <StScriptCharName>〈{charName}〉</StScriptCharName>}
+      <StScriptText>{currentText}</StScriptText>
+    </StScriptField>
+  );
+};
+
+const StScriptField = styled.div`
+  margin: 0 1% 1% 1%;
+  padding: 1%;
+  height: 84%;
+  border: 1px solid black;
+
+  display: flex;
+  gap: 20px;
+
+  font-size: 32px;
+`;
+
+const StScriptCharName = styled.div`
+  font-weight: bold;
+`;
+
+const StScriptText = styled.span``;
+
+export default ScriptDisplay;
